95316782c14af6897ff5dfb73296619931cd9973
keisuke-333 <keikubo.999@gmail.com>
2024-07-15 19:14:14 +0900

    setup nativewind

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,11 +1,11 @@
 import { View, Text } from "react-native"
 
-const index = () => {
+const Home = () => {
   return (
-    <View>
-      <Text>トップ画面</Text>
+    <View className="flex-1 items-center justify-center">
+      <Text className="text-3xl text-red-600">トップ画面</Text>
     </View>
   )
 }
 
-export default index
+export default Home
