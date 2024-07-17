3e50b321c88f7608463972e75ae2f0e0c5100896
keisuke-333 <keikubo.999@gmail.com>
2024-07-18 00:06:07 +0900

    setup google font

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,6 +1,29 @@
-import { Stack } from "expo-router"
+import { useEffect } from "react"
+import { SplashScreen, Stack } from "expo-router"
+import {
+  useFonts,
+  NotoSansJP_400Regular,
+  NotoSansJP_700Bold,
+} from "@expo-google-fonts/noto-sans-jp"
+
+SplashScreen.preventAutoHideAsync()
 
 export const RootLayout = () => {
+  const [fontsLoaded, fontError] = useFonts({
+    NotoSansJP_400Regular,
+    NotoSansJP_700Bold,
+  })
+
+  useEffect(() => {
+    if (fontsLoaded || fontError) {
+      SplashScreen.hideAsync()
+    }
+  }, [fontsLoaded, fontError])
+
+  if (!fontsLoaded && !fontError) {
+    return null
+  }
+
   return (
     <Stack>
       <Stack.Screen name="index" options={{ headerShown: false }} />
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,10 @@ module.exports = {
     "./src/components/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    fontFamily: {
+      "noto-reqular": ["NotoSansJP_400Regular", "sans-serif"],
+      "noto-bold": ["NotoSansJP_700Bold", "sans-serif"],
+    },
     extend: {},
   },
   plugins: [],
